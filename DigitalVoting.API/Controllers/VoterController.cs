2d0d377ec30e59ee5ac048a0d4fa4cd969114d7a
AllanDutra <allandutrafeitosa14@gmail.com>
2023-02-25 13:31:42 -0300

    feat: sign-up route

---
+++ DigitalVoting.API/Controllers/VoterController.cs
@@ -0,0 +1,25 @@
+using DigitalVoting.Application.Commands.SignUp;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalVoting.API.Controllers
+{
+    [ApiController]
+    [Route("api/Voter")]
+    public class VoterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public VoterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> SignUpAsync([FromBody] SignUpCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok(new { message = "Successfully registered Voter!" });
+        }
+    }
+}
